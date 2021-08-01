83fafe54896704e0c889edbba8a6dc625851cef3
Yehudit Shreiber <yehudit373@gmail.com>
2021-08-01 22:34:46 +0300

    yehudit's commit- before ronit changes

--- src/main/java/com/mycompany/mavenproject2/Pages/ManagedBeans/ShowDetailsBean.java
+++ src/main/java/com/mycompany/mavenproject2/Pages/ManagedBeans/ShowDetailsBean.java
@@ -10,6 +10,7 @@ import com.mycompany.mavenproject2.DataLayer.Show;
 import com.mycompany.mavenproject2.Services.SeatsService;
 import com.mycompany.mavenproject2.Services.ShowDetailsService;
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 import javax.faces.view.ViewScoped;
 import javax.inject.Named;
@@ -26,7 +27,65 @@ public class ShowDetailsBean implements Serializable {
 
     int [][] seats;
     
+    private List<List<Float>> _Matrix;
+
+    private int _ColumnNumber;
+    
+    public int get_ColumnNumber(){
+        return _Matrix.size();
+    }
+
+    public List<Integer> getRows(){
+        List<Integer> y = new ArrayList();
+        int i = 0;
+        while(i < selectedShow.getRows()){
+            y.add(++i);
+        }
+        return y;
+    }
+   
+    
+        public List<Integer> getCols(){
+         List<Integer> y = new ArrayList();
+        int i = 0;
+        while(i < selectedShow.getColumns()){
+            y.add(++i);
+        }
+        return y;
+    }
+    
+    
+    public int[] getRowSeats(int[] row){
+        int u[] = new int[9];
+        return u;
+    }
+    
+        public int[] getRowSeats(){
+            seats[0][0] = 8;
+            seats[0][1] = 4;
+        return seats[0];
+    }
+    
+    
+public List<List<Float>> get_Matrix() {
+    return this._Matrix;
+}
+    
+    
     public Show getSelectedShow() {
+        this._Matrix = new ArrayList<List<Float>>();
+    this._Matrix.add(new ArrayList<Float>());
+    this._Matrix.add(new ArrayList<Float>());
+    this._Matrix.get(0).add(1.0f);
+    this._Matrix.get(0).add(2.0f);
+    this._Matrix.get(0).add(3.0f);
+    this._Matrix.get(1).add(1.0f);
+    this._Matrix.get(1).add(2.0f);
+    this._Matrix.get(1).add(3.0f);
+        
+        
+        
+        
         if(selectedShow== null){
              this.selectedShow = ShowDetailsService.getShow();
         this.seats = new int [selectedShow.getRows()][selectedShow.getColumns()];
