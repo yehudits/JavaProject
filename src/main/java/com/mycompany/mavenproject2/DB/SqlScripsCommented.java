364202e673b95223badfda965f5beedc89888299
Yehudit Shreiber <yehudit373@gmail.com>
2021-08-22 18:25:28 +0300

    not important commit- commented create table query

---
+++ src/main/java/com/mycompany/mavenproject2/DB/SqlScripsCommented.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.mavenproject2.DB;
+
+/**
+ *
+ * @author HP
+ */
+public class SqlScripsCommented {
+    
+    
+//    CREATE TABLE "user" (
+//    "id" INTEGER not null primary key generated always as identity,
+//    "name" VARCHAR(50),
+//    "email" VARCHAR(50),
+//    "password" varchar (50),
+//    "userType" boolean
+//);
+//
+//    
+}
